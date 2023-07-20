8509c5470e6378d12dcae519c4055dd55cb07d2d
Jun <rnsuz74@naver.com>
2023-07-20 23:05:17 +0900

    React Native 1일차

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>어플을 만들어 볼까?</Text>
+      <StatusBar style="light" /> 
     </View>
   );
 }
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 40,
+    color: 'yellow',
+  }
 });
