7c3eefef77a98f9ca450f71adcae8896f8b7dd62
Jun <rnsuz74@naver.com>
2023-07-21 19:59:46 +0900

    css(flex,ScrollView,Dimensions)

--- App.js
+++ App.js
@@ -1,28 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+
+const {width:SCREEN_WIDTH} = Dimensions.get("window"); //Dimensions : 해당 핸드폰의 screen size를 알수 있는 API
+
+console.log(SCREEN_WIDTH);
 
 export default function App() {
   return (
-    <View style={{flex:1, flexDirection: 'row'}}>
     <View style={styles.container}>
-      <Text style={styles.text}>어플을 만들어 볼까?</Text>
-    </View>
-    <View style={{flex:1, backgroundColor: 'tomato'}}></View>
-    <View style={{flex:1, backgroundColor: 'orange'}}></View>
-    <View style={{flex:1, backgroundColor: 'blue'}}></View>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView 
+      horizontal
+      pagingEnabled
+      showsHorizontalScrollIndicator="false" 
+      contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>34</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>34</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>34</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>34</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>  
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1.5,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1, 
+    backgroundColor: 'hotpink',
   },
-  text: {
-    fontSize: 40,
-    color: 'yellow',
-  }
-});
+  city:{
+    flex:1.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName:{
+    fontSize: 68,
+    fontWeight: '600',
+  },
+  weather:{
+    
+  },
+  day:{
+    width: SCREEN_WIDTH,
+    alignItems:'center',
+  },
+  temp:{
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description:{
+    marginTop: -30,
+    fontSize: 60,
+  },
+})
+
